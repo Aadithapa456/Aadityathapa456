730cd545d4fedc4ba33f16ba2fbb49434cbd4465
Aaditya Thapa <81004769+Aadithapa456@users.noreply.github.com>
2022-07-21 14:58:05 +0545

    First version

---
+++ Connect.php
@@ -0,0 +1,20 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+
+if($conn)
+{
+    echo "Connection OK";
+}
+else{
+    echo "Connection Failed".mysqli_connect_eror;
+}
+
+
+
+?>
